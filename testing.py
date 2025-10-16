380ff6da13af62582a5c2ff93d59965df8beeb10
michaelkosko <michael.kosko@thinkiq.com>
2025-10-15 19:24:49 -0700

    testing code

---
+++ testing.py
@@ -0,0 +1,17 @@
+import serial_connection
+import time
+
+aprs_stream = serial_connection.SerialTTY()
+
+try:
+    print(f"visible ports : {aprs_stream.available_ports}")
+
+    aprs_stream.start_streaming()
+    while aprs_stream._running:
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    print("\n user interrupt")
+finally:
+    aprs_stream.stop_streaming()
+    
