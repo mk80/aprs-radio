5b7cc3436aef9680ec9d2115120eb2db064735dd
michaelkosko <michael.kosko@thinkiq.com>
2026-01-08 06:04:25 -0800

    still building aprs-is class

--- aprs-is.py
+++ aprs-is.py
@@ -2,11 +2,12 @@ import aprslib
 
 class IGateway:
 
-    def __init__(self):
-        server = "rotate.aprs2.net"
-        port = 14580
+    def __init__(self, call):
+        self.server = "rotate.aprs2.net"
+        self.port = 14580
         token_file = './cs_token'
-        passcode = ''
+        self.passcode = ''
+        self.callsign = call
 
         try:
             with open(token_file,'r') as f:
@@ -16,5 +17,12 @@ class IGateway:
             print(f"ERROR : '{token_file}' was not found")
         except Exception as err:
             print(f"ERROR : '{err}' : unexpected error reading file")
+        
+        try:
+            self.aprs = aprslib.IS(self.callsign, passwd=self.passcode, host=self.server, port=self.port)
+            self.aprs.connect()
+        except Exception as err:
+            print(f"ERROR : '{err}' : unexpected error connecting to aprs-is")
+            
 
     
