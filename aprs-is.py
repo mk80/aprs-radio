b812b478116228acc7b66b88a1357a46553ca9bf
michaelkosko <michael.kosko@thinkiq.com>
2026-01-10 13:40:07 -0800

    adding new function

--- aprs-is.py
+++ aprs-is.py
@@ -24,5 +24,23 @@ class IGateway:
         except Exception as err:
             print(f"ERROR : '{err}' : unexpected error connecting to aprs-is")
             
+    def gate_to_internet(self, raw_packet_string):
+        """
+        Sending packet to APRS-IS
+        
+        :param self: self reference
+        :param raw_packet_string: TNC2 formatted string :: 'CALL>DEST,PATH:PAYLOAD'
+        """
+        try:
+            self.aprs.sendall(raw_packet_string)
+            print(f"uploaded to APRS-IS :: {raw_packet_string}")
+        except Exception as err:
+            print(f"ERROR :: failed to upload :: {err}")
 
-    
+    def disconnect(self):
+        """
+        Disconnect from APRS-IS server
+        
+        :param self: self reference
+        """
+        self.aprs.close()
